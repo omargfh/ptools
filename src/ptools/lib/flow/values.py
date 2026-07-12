29ed176a47a70146ee8f25888c7ae12fb3bfcd66
Omar Ibrahim <omaribrahim@uchicago.edu>
2026-07-12 16:26:24 -0500

    refactor(flow): reuse shared output flavors from utils.output
    - values.py re-exports the flavor machinery instead of duplicating it
    - keeps StreamValue/InputFlavorKind as the flow-specific parts

--- src/ptools/lib/flow/values.py
+++ src/ptools/lib/flow/values.py
@@ -1,9 +1,8 @@
 from enum import Enum
-from abc import ABC, abstractmethod
+
+from ptools.utils.output import *
 
 from .grammar import StreamTransformer, parser
-from ptools.utils.decorator_compistor import DecoratorCompositor
-
 
 # Input
 class StreamValue:
@@ -18,64 +17,7 @@ class StreamValue:
     def __repr__(self):
         return repr(self.value)
 
-# Output
-class OutputFlavorKind(Enum):
-    plain = 'plain'
-    json = 'json'
-    python = 'python'
-    unflavored = 'unflavored'
-    none = 'none'
-
 class InputFlavorKind(Enum):
     python_like = 'python-like'
     json = 'json'
     python = 'python'
-
-class OutputFlavor(ABC):
-    @abstractmethod
-    def format(self, value):
-        pass
-
-class OutputPlainFlavor(OutputFlavor):
-    def format(self, value):
-        if isinstance(value, list):
-            return '\n'.join(str(v) for v in value)
-        elif isinstance(value, dict):
-            return '\n'.join(f"{k}: {v}" for k, v in value.items())
-        else:
-            return str(value)
-
-class OutputJSONFlavor(OutputFlavor):
-    def format(self, value):
-        import json
-        return json.dumps(value, indent=2)
-
-class OutputPythonFlavor(OutputFlavor):
-    def format(self, value):
-        return repr(value)
-
-class OutputNoneFlavor(OutputFlavor):
-    def format(self, value):
-        return ''
-
-class OutputUnflavoredFlavor(OutputFlavor):
-    def format(self, value):
-        return str(value)
-
-class OutputValue:
-    def __init__(self, flavor: OutputFlavorKind = OutputFlavorKind.plain):
-        if flavor == OutputFlavorKind.plain:
-            self.flavor = OutputPlainFlavor()
-        elif flavor == OutputFlavorKind.json:
-            self.flavor = OutputJSONFlavor()
-        elif flavor == OutputFlavorKind.python:
-            self.flavor = OutputPythonFlavor()
-        elif flavor == OutputFlavorKind.none:
-            self.flavor = OutputNoneFlavor()
-        elif flavor == OutputFlavorKind.unflavored:
-            self.flavor = OutputUnflavoredFlavor()
-        else:
-            raise ValueError(f"Unknown flavor: {flavor}")
-
-    def format(self, value):
-        return self.flavor.format(value)
